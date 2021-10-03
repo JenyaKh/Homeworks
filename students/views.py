6270b60102b9b48dc54c8e50caade5c8506f91b1
EKhapchenko <ekhapchenko@gmail.com>
2021-10-03 20:44:44 +0300

    moved variable fake

--- students/views.py
+++ students/views.py
@@ -8,8 +8,8 @@ def hello(request):
 
 
 def generate_students(request, count=10):
+    fake = faker.Faker('ru_RU')
     while count > 0:
-        fake = faker.Faker('ru_RU')
         new_student = Student(first_name=fake.first_name(), last_name=fake.last_name(), email=fake.email())
         new_student.save()
         count -= 1
